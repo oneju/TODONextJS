31632bf57385964712f17a0c48d13894369118ef
oneju <wonju.noh.24@gmail.com>
2024-01-05 15:30:10 +0900

    Fix: add todo response error

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,6 +1,5 @@
-import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
-import { useSession } from "next-auth/react";
 
 interface RequestBody {
   content: string;
@@ -10,7 +9,12 @@ export async function GET() {
   return new Response(JSON.stringify(todos));
 }
 
-export async function POST(req: any) {
-  const { data } = await req.json();
-  await prisma.todo.create({ data: { content: data } });
+export async function POST(req: Request, res: NextResponse) {
+  try {
+    const { data } = await req.json();
+    await prisma.todo.create({ data: { content: data } });
+    return NextResponse.json(data);
+  } catch (error) {
+    return res.json();
+  }
 }
